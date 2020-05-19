01ac3beb5b5924eb1c88b94003ea9f5903321bf2
maydra <65583691+maydra@users.noreply.github.com>
2020-05-19 15:02:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random ##運勢リストからランダムに出力するために必要
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,11 +13,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
     
- if message.content == "占い":
+       if message.content == "占い":
 #レスポンスされる運勢のリストを作成
         unsei = ["大吉", "中吉", "吉", "末吉", "小吉", "凶", "大凶"]
-        choice = random.choice(unsei) #randomモジュールでunseiリストからランダムに一つを選出
-        await message.send_message(message.channel, choice) #結果を出力
+        choice = random.choice(unsei) #randomモジュール使用
+        await message.send_message(message.channel, choice)
 
 @bot.command()
 async def ping(ctx):
