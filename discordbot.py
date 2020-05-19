26ebff3c83edb94bd0e81bda7a243a0cc1985535
maydra <65583691+maydra@users.noreply.github.com>
2020-05-19 14:59:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,12 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
+    
+ if message.content == "占い":
+#レスポンスされる運勢のリストを作成
+        unsei = ["大吉", "中吉", "吉", "末吉", "小吉", "凶", "大凶"]
+        choice = random.choice(unsei) #randomモジュールでunseiリストからランダムに一つを選出
+        await message.send_message(message.channel, choice) #結果を出力
 
 @bot.command()
 async def ping(ctx):
